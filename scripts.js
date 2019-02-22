5710ba1fa0312376388137ea2e3da1b4fa710623
stvkrx <mburu@btimillman.com>
2019-02-22 14:30:08 +0300

    added toggle function to memory card class

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 const cards = document.querySelectorAll('.memory-card');
 
 function flipCard() {
-	console.log('I was clicked!');
-	console.log(this);
+    this.classList.toggle('flip');
+   
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
