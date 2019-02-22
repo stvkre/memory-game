70ffd9b96bb12d7683de98094551f555f2c493b5
stvkrx <mburu@btimillman.com>
2019-02-22 17:14:55 +0300

    randomised the order of the cards with an immediately invoked function expression

--- scripts.js
+++ scripts.js
@@ -7,6 +7,7 @@ let firstCard, secondCard;
 
 function flipCard() {
     if (lockBoard) return;
+    if (this === firstCard) return;
     //
     this.classList.add('flip');
 
@@ -39,6 +40,8 @@ function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
 
+    resetBoard();
+
 }
 
 function unflipCards() {
@@ -48,10 +51,24 @@ function unflipCards() {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
 
-    lockBoard = false;
+        resetBoard();
     }, 1500);
 
 }
+
+function resetBoard() {
+    // desturcturing
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard.secondCard] = [null, null];
+}
+
+// shuffling the order of the cards with an Immediately Invoked Function Expression (IIFE)
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    });
+})();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
