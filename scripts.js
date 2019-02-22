2815dca57ca91ceed7b87ddad771461bf4c6fa5a
stvkrx <mburu@btimillman.com>
2019-02-22 15:15:47 +0300

    set instances to check the click status of the memory card

--- scripts.js
+++ scripts.js
@@ -1,7 +1,24 @@
 const cards = document.querySelectorAll('.memory-card');
 
+// checking if memory card is clicked
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
-    this.classList.toggle('flip');
+    this.classList.add('flip');
+
+    // setting card click status
+    if (!hasFlippedCard) {
+        // clicked first time
+        hasFlippedCard = true;
+        firstCard = this;
+    } else {
+        // clicked second time
+        hasFlippedCard = false;
+        secondCard = this;
+
+    
+    }
    
 }
 
